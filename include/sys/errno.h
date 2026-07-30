786d78de9d451d2ea1e9f0fed0d42e03bc3ef9c2
Serge Vakulenko <serge.vakulenko@gmail.com>
2026-07-29 23:48:58 -0700

    Explain errno values in comments.

--- include/sys/errno.h
+++ include/sys/errno.h
@@ -29,42 +29,42 @@
 #ifndef _SYS_ERRNO_H
 #define _SYS_ERRNO_H
 
-#define EPERM   1
-#define ENOENT  2
-#define ESRCH   3
-#define EINTR   4
-#define EIO     5
-#define ENXIO   6
-#define E2BIG   7
-#define ENOEXEC 8
-#define EBADF   9
-#define ECHILD  10
-#define EAGAIN  11
-#define ENOMEM  12
-#define EACCES  13
-#define EFAULT  14
-#define ENOTBLK 15
-#define EBUSY   16
-#define EEXIST  17
-#define EXDEV   18
-#define ENODEV  19
-#define ENOTDIR 20
-#define EISDIR  21
-#define EINVAL  22
-#define ENFILE  23
-#define EMFILE  24
-#define ENOTTY  25
-#define ETXTBSY 26
-#define EFBIG   27
-#define ENOSPC  28
-#define ESPIPE  29
-#define EROFS   30
-#define EMLINK  31
-#define EPIPE   32
+#define EPERM   1   // Not owner
+#define ENOENT  2   // No such file or directory
+#define ESRCH   3   // No such process
+#define EINTR   4   // Interrupted system call
+#define EIO     5   // I/O error
+#define ENXIO   6   // No such device or address
+#define E2BIG   7   // Arg list too long
+#define ENOEXEC 8   // Exec format error
+#define EBADF   9   // Bad file number
+#define ECHILD  10  // No children
+#define EAGAIN  11  // No more processes
+#define ENOMEM  12  // Not enough core
+#define EACCES  13  // Permission denied
+#define EFAULT  14  // Bad address
+#define ENOTBLK 15  // Block device required
+#define EBUSY   16  // Mount device busy
+#define EEXIST  17  // File exists
+#define EXDEV   18  // Cross-device link
+#define ENODEV  19  // No such device
+#define ENOTDIR 20  // Not a directory
+#define EISDIR  21  // Is a directory
+#define EINVAL  22  // Invalid argument
+#define ENFILE  23  // File table overflow
+#define EMFILE  24  // Too many open files
+#define ENOTTY  25  // Not a typewriter
+#define ETXTBSY 26  // Text file busy
+#define EFBIG   27  // File too large
+#define ENOSPC  28  // No space left on device
+#define ESPIPE  29  // Illegal seek
+#define EROFS   30  // Read-only file system
+#define EMLINK  31  // Too many links
+#define EPIPE   32  // Broken pipe
 
 // The C11 mandatory three -- library codes, never assigned by the kernel.
-#define EDOM   33
-#define ERANGE 34
-#define EILSEQ 35
+#define EDOM   33   // Argument too large
+#define ERANGE 34   // Result too large
+#define EILSEQ 35   // Illegal byte sequence
 
 #endif // _SYS_ERRNO_H
